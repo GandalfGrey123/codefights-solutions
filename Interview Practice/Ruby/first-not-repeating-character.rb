6e4c81de5d4077e871219d14bff07437801c13a1
Marcus <gandalfgrey@gmx.com>
2019-06-29 18:04:52 -0700

    Create first-not-repeating-character.rb

---
+++ Interview Practice/Ruby/first-not-repeating-character.rb
@@ -0,0 +1,12 @@
+def firstNotRepeatingCharacter(s)
+    seen={}
+    counts = s.delete(' ').each_char.inject(Hash.new(0)) {|a,c| a[c] += 1; a} 
+    x = counts.key(1)
+    
+    if x != nil
+        return x
+    else 
+        return '_'
+    end
+end
+
